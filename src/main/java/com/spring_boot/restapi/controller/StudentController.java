c7551f3b18e9764920f1c896f11b0d46cc5e2a3d
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-13 19:29:49 +0700

    Spring Boot DELETE REST API with @DeleteMapping annotation

--- src/main/java/com/spring_boot/restapi/controller/StudentController.java
+++ src/main/java/com/spring_boot/restapi/controller/StudentController.java
@@ -73,4 +73,12 @@ public class StudentController {
         System.out.println(student.getLastName());
         return student;
     }
+
+    // Spring Boot REST API that handles HTTP DELETE Request - deleting the existing resource
+
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully.!";
+    }
 }
