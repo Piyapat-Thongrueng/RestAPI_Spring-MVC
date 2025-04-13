74838fa18bdd2858c4c67d79ad0fe64e8347286c
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-13 16:32:57 +0700

    Spring Boot REST API returns List in JSON Format

--- src/main/java/com/spring_boot/restapi/controller/StudentController.java
+++ src/main/java/com/spring_boot/restapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.spring_boot.restapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -16,5 +19,18 @@ public class StudentController {
                 "Thongrueng"
         );
         return student;
+    }
+
+    //http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student>getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Will", "Smiths"));
+        students.add(new Student(2, "Josh", "Lucas"));
+        students.add(new Student(3, "Peter", "Parker"));
+        students.add(new Student(4, "Harry", "Potter"));
+        students.add(new Student(5, "John", "Corner"));
+
+        return students;
     }
 }
