d2a4cc56e70d0caec050b8b57870365fc2c6a72c
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-13 17:21:16 +0700

    Spring Boot REST API with @RequestParam annotation

--- src/main/java/com/spring_boot/restapi/controller/StudentController.java
+++ src/main/java/com/spring_boot/restapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.spring_boot.restapi.controller;
 import com.spring_boot.restapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -43,5 +44,15 @@ public class StudentController {
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring BOOT REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=Jabby&lastName=Helloworld
+
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
     }
 }
