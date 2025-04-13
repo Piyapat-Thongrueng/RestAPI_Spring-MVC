372ddb5c6b1ae80d211c2f2c16443202d3840383
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-13 15:30:39 +0700

    Create simple Spring Boot REST API

---
+++ src/main/java/com/spring_boot/restapi/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.spring_boot.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //HTTP GET Request
+    //http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello world!!";
+    }
+
+
+}
